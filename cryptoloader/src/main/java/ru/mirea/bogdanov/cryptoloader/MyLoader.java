f9f040cfd55c0ea3a5b2b5059a7c513cba866aaf
demonbog <d.bogdanov2002@yandex.ru>
2023-04-28 13:12:35 +0300

    first comit

--- cryptoloader/src/main/java/ru/mirea/bogdanov/cryptoloader/MyLoader.java
+++ cryptoloader/src/main/java/ru/mirea/bogdanov/cryptoloader/MyLoader.java
@@ -7,26 +7,49 @@ import android.os.SystemClock;
 import androidx.annotation.NonNull;
 import androidx.loader.content.AsyncTaskLoader;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class MyLoader extends AsyncTaskLoader<String> {
     private String firstName;
     public static final String ARG_WORD = "word";
-
     public MyLoader(@NonNull Context context, Bundle args) {
         super(context);
-        if (args != null)
-            firstName = args.getString(ARG_WORD);
+        byte[] cryptText = new byte[0];
+        if (args != null) {
+            cryptText = args.getByteArray(ARG_WORD);
+            byte[] key = args.getByteArray("key");
+            SecretKey originalKey = new SecretKeySpec(key, 0, key.length, "AES");
+            firstName = decryptMsg(cryptText, originalKey);
+        }
     }
-
     @Override
     protected void onStartLoading() {
         super.onStartLoading();
         forceLoad();
     }
-
     @Override
     public String loadInBackground() {
-// emulate long-running operation
+        // emulate long-running operation
         SystemClock.sleep(5000);
         return firstName;
+    }
+    public static String decryptMsg(byte[] cipherText, SecretKey secret){
+        /* Decrypt the message */
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secret);
+            return new String(cipher.doFinal(cipherText));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+                 | BadPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ workmanager/src/main/java/ru/mirea/bogdanov/workmanager/MainActivity.java
@@ -0,0 +1,45 @@
+package ru.mirea.bogdanov.workmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+import android.os.Bundle;
+import android.view.View;
+
+import ru.mirea.bogdanov.workmanager.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        WorkRequest uploadWorkRequest =
+                new OneTimeWorkRequest.Builder(UploadWorker.class)
+                        .build();
+        WorkManager
+                .getInstance(this)
+                .enqueue(uploadWorkRequest);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Constraints constraints = new Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.UNMETERED)
+                        .setRequiresCharging(true)
+                        .build();
+                WorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
+                        .setConstraints(constraints)
+                        .build();
+            }
+        });
+    }
+
+}
