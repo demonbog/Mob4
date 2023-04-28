a3379025daa69698cb20ce9c8bad969be7c5ef0f
demonbog <d.bogdanov2002@yandex.ru>
2023-04-28 11:04:55 +0300

    first commit

---
+++ thread/src/main/java/ru/mirea/bogdanov/thread/MainActivity.java
@@ -0,0 +1,52 @@
+package ru.mirea.bogdanov.thread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Arrays;
+
+import ru.mirea.bogdanov.thread.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Thread mainThread = Thread.currentThread();
+        binding.textView.setText("Имя текущего потока: " + mainThread.getName());
+// Меняем имя и выводим в текстовом поле
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 03, НОМЕР ПО СПИСКУ: 04, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: Гарри Поттер");
+        binding.textView.append("\n Новое имя потока: " + mainThread.getName());
+        Log.d(MainActivity.class.getSimpleName(), "Stack: " + Arrays.toString(mainThread.getStackTrace()));
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        int numberThread = counter++;
+                        Log.d("ThreadProject", String.format("Запущен поток No %d студентом группы No %s номер по списку No %d ", numberThread, "БСБО-03-20", 4));
+                        long endTime = System.currentTimeMillis() + 20 * 1000;
+                        while (System.currentTimeMillis() < endTime) {
+                            synchronized (this) {
+                                try {
+                                    wait(endTime - System.currentTimeMillis());
+                                    Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            Log.d("ThreadProject", "Выполнен поток No " + numberThread);
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+}
